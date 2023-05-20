e50ef3ffab423977684bc69d717fc0baba7032fc
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-19 23:29:06 -0300

    Add files via upload

--- Programação Orientada a Objetos/Conta.py
+++ Programação Orientada a Objetos/Conta.py
@@ -1,15 +1,27 @@
 class conta:
-    def __init__(self,numero,titular,conta,saldo,limite = 1000):
+    def __init__(self, numero, titular, conta, saldo, limite=1000):
         self.numero = numero
         self.titular = titular
         self.conta = conta
         self.saldo = saldo
         self.limite = limite
 
+    def Extrato(self):
+        print(f"Titular: {self.titular}\nConta: {self.conta}\nSaldo: R${self.saldo}")
 
+    def Depositar(self, valor):
+        self.saldo += valor
 
-conta1 = conta(222,"Tiago",212,90000)#variável nomeia-se "referência"
-conta2 = conta(223,"Manoel Gomes",213,1000000)
-conta3 = conta(224,"Thais Carla",214,100,500)
+    def Sacar(self,valor):
+        self.saldo -= valor
 
-print(f"Saldo: R${conta2.saldo}")
+conta1 = conta(222, "Tiago", 212, 10)  # variável nomeia-se "referência"
+conta2 = conta(223, "Manoel Gomes", 213, 1000000)
+conta3 = conta(224, "Thais Carla", 214, 100, 500)
+
+deposito = int(input("Digite o valor que deseja depositar: R$"))
+
+conta1.Extrato()
+conta1.Sacar(deposito)
+conta1.Extrato()
+
