6e7b318faedc65e43716f9c1d0dd1f26633c53e2
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-20 00:28:14 -0300

    Add files via upload

--- Programação Orientada a Objetos/Conta.py
+++ Programação Orientada a Objetos/Conta.py
@@ -1,3 +1,5 @@
+import datetime
+
 class conta:
     def __init__(self, numero, titular, conta, saldo, limite=1000):
         self.numero = numero
@@ -7,21 +9,23 @@ class conta:
         self.limite = limite
 
     def Extrato(self):
-        print(f"Titular: {self.titular}\nConta: {self.conta}\nSaldo: R${self.saldo}")
+        print(f"Titular: {self.titular}\nConta: {self.conta}\nSaldo: R${self.saldo}\nData do extrato: "
+              f"{datetime.datetime.now().strftime('%x')}\nHora do extrato: {datetime.datetime.now().strftime('%I')}:"
+              f"{datetime.datetime.now().strftime('%M')}{datetime.datetime.now().strftime('%p').lower()}")
 
     def Depositar(self, valor):
         self.saldo += valor
 
-    def Sacar(self,valor):
+    def Sacar(self, valor):
         self.saldo -= valor
 
-conta1 = conta(222, "Tiago", 212, 10)  # variável nomeia-se "referência"
+
+nomeTitular = input("Digite o nome completo do titular: ").title()
+conta1 = conta(222, nomeTitular, 212, 10)  # variável nomeia-se "referência"
 conta2 = conta(223, "Manoel Gomes", 213, 1000000)
 conta3 = conta(224, "Thais Carla", 214, 100, 500)
 
-deposito = int(input("Digite o valor que deseja depositar: R$"))
-
+deposito = int(input(f"Olá, {conta1.titular}!\nDigite o valor que deseja depositar: R$"))
+conta1.Depositar(deposito)
 conta1.Extrato()
-conta1.Sacar(deposito)
-conta1.Extrato()
-
+outraRef = None#aponta para nenhuma referência
