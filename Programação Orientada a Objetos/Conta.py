d142e2222315a302b681b2b1a6d8bc8aa7129b1f
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-23 23:55:19 -0300

    Add files via upload

--- Programação Orientada a Objetos/Conta.py
+++ Programação Orientada a Objetos/Conta.py
@@ -58,22 +58,5 @@ class Conta:
               f"{self.get_limite()} para R${self.set_limite(valor)}")
 
 
-nomeTitular = input("Digite o nome completo do titular: ").title()
-conta1 = Conta(222, nomeTitular, 212, 10)  # variável nomeia-se "referência"
-conta2 = Conta(223, "Manoel Gomes", 213, 1000000)
-conta3 = Conta(224, "Thais Carla", 214, 100, 500)
-
-deposito = int(input(f"Olá, {conta1.titular}!\nDigite o valor que deseja "
-                     f"depositar: R$"))
-conta1.Depositar(deposito)
-conta1.Extrato()
-
-valorRecebido = float(input("Digite o valor que deseja transferir: R$"))
-conta2.Transferir(valorRecebido, conta1)  # origem.Transferir(<valor>,<destino>)
-
-conta2.Transferencia(valorRecebido)
-conta2.Extrato()
-
-conta3.AumentoDeLimite(1500)
 
 outraRef = None  # aponta para nenhuma referência
