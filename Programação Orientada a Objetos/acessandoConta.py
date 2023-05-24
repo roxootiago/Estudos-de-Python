2d46e87b97480fc96da8e727686f17aa3e59be89
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-24 17:20:15 -0300

    Add files via upload

--- Programação Orientada a Objetos/acessandoConta.py
+++ Programação Orientada a Objetos/acessandoConta.py
@@ -3,19 +3,21 @@ from Conta import *
 nomeTitular = input("Digite o nome completo do titular: ").title()
 
 conta1 = Conta(222, nomeTitular, 212, 10)  # variável nomeia-se "referência"
-conta2 = Conta(223, "Manoel Gomes", 213, 1000000)
-conta3 = Conta(224, "Thais Carla", 214, 100, 500)
+conta2 = Conta(223, "Manoel Gomes", 213, 9000)
+conta3 = Conta(224, "Jailson Mendes", 214, 100, 500)
 
-deposito = int(input(f"Olá, {conta1.titular}!\nDigite o valor que deseja "
-                     f"depositar: R$"))
+deposito = int(input(f"Olá, {conta1.titular}!\nDigite o valor que deseja depositar: R$"))
 conta1.Depositar(deposito)
 conta1.Extrato()
 
 valorRecebido = float(input("Digite o valor que deseja transferir: R$"))
 conta2.Transferir(valorRecebido, conta1)  # origem.Transferir(<valor>,<destino>)
 
-conta2.Transferencia(valorRecebido)
 conta2.Extrato()
+conta3.AumentoDeLimite(900)
 
-conta3.limite
+conta1.Extrato()
+conta1.Sacar(10)
+conta1.Extrato()
 
+print(f"Titular: {conta1.titular}\nCodigo do banco: {conta1.CodigoBanco()}")
