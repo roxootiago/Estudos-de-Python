158effd15671a311c1d60c7483c6c8a21e6ec34e
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-23 23:23:33 -0300

    Add files via upload

--- Programação Orientada a Objetos/Conta.py
+++ Programação Orientada a Objetos/Conta.py
@@ -10,10 +10,17 @@ class Conta:
         self.__limite = limite
 
     def Extrato(self):
-        print(f"{'-'*50}\n{' '*11}Bem vindo ao Banco Python\n{'-'*50}\n"
-              f"Titular: {self.titular}\nConta: {self.__conta}\nSaldo: R${self.__saldo}\nData do extrato: "
-              f"{datetime.datetime.now().strftime('%x')}\nHora do extrato: {datetime.datetime.now().strftime('%I')}:"
-              f"{datetime.datetime.now().strftime('%M')}{datetime.datetime.now().strftime('%p').lower()}")
+        print(f"{'-' * 50}\n{' ' * 11}Bem vindo ao Banco Python\n{'-' * 50}\n"
+              f"Titular: {self.titular}\nConta: {self.__conta}\nSaldo: R$"
+              f"{self.__saldo}\nData do extrato:"
+              f"{datetime.datetime.now().strftime('%d')}/"
+              f"{datetime.datetime.now().strftime('%m')}/"
+              f"{datetime.datetime.now().strftime('%Y')}\nHora "
+              f"do "
+              f"extrato: "
+              f"{datetime.datetime.now().strftime('%I')}:"
+              f"{datetime.datetime.now().strftime('%M')}"
+              f"{datetime.datetime.now().strftime('%p').lower()}\n")
 
     def Depositar(self, valor):
         self.__saldo += valor
@@ -21,9 +28,22 @@ class Conta:
     def Sacar(self, valor):
         self.__saldo -= valor
 
-    def Transferir(self,valor,destino):
+    def Transferir(self, valor, destino):
         self.Sacar(valor)
         destino.Depositar(valor)
+
+    def Transferencia(self, valor):
+        print(f"{'-' * 50}\n{' ' * 11}Bem vindo ao Banco Python\n{'-' * 50}\n"
+              f"Você recebeu uma transferência de {self.titular.upper()}"
+              f"\nValor"
+              f" recebido: R${valor}\nData da transferência:"
+              f"{datetime.datetime.now().strftime('%d')}/"
+              f"{datetime.datetime.now().strftime('%m')}/"
+              f"{datetime.datetime.now().strftime('%Y')}\nHora "
+              f"da transferência: "
+              f"{datetime.datetime.now().strftime('%I')}:"
+              f"{datetime.datetime.now().strftime('%M')}"
+              f"{datetime.datetime.now().strftime('%p').lower()}\n")
 
 
 nomeTitular = input("Digite o nome completo do titular: ").title()
@@ -31,13 +51,15 @@ conta1 = Conta(222, nomeTitular, 212, 10)  # variável nomeia-se "referência"
 conta2 = Conta(223, "Manoel Gomes", 213, 1000000)
 conta3 = Conta(224, "Thais Carla", 214, 100, 500)
 
-deposito = int(input(f"Olá, {conta1.titular}!\nDigite o valor que deseja depositar: R$"))
+deposito = int(input(f"Olá, {conta1.titular}!\nDigite o valor que deseja "
+                     f"depositar: R$"))
 conta1.Depositar(deposito)
 conta1.Extrato()
 
-conta1._Conta__saldo = 50#evitar fazer isso
-conta1.Extrato()
-conta2.Transferir(2000,conta1)#origem.Transferir(<valor>,<destino>)
-conta1.Extrato()
+valorRecebido = float(input("Digite o valor que deseja transferir: R$"))
+conta2.Transferir(valorRecebido, conta1)  # origem.Transferir(<valor>,<destino>)
+
+conta2.Transferencia(valorRecebido)
+conta2.Extrato()
 
 outraRef = None  # aponta para nenhuma referência
